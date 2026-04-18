8d6c2a347242bfd54560cc2c47f5f552054d978b
faheyf250 <faheyf250@gmail.com>
2026-04-18 01:36:48 +0000

    moved code from main.py to routes.py. Changed name of recipe.html to recipes.html for greater clarity as well as changed function name in routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,35 @@ from app import app, db
 from app.models import Recipe
 from app.forms import RecipeForm
 
-@app.route('/dashboard')
-def dashboard():
+
+
+@app.route("/")
+def home():
+    return render_template("homepage.html")
+
+#Pagee that displays all recipes
+@app.route("/recipes")
+def recipes():
     recipes = Recipe.query.all()
-    return render_template('base.html', recipes=recipes)
+    #Code I have added for now until database is in place. This is only a temporary fix that will be removed.
+    for r in recipes: 
+        formatted_ingredients = []
+        for item in r.ingredients.split(' | '):
+            parts = item.split(' ', 2) 
+
+            formatted_ingredients.append({
+                'quantity': parts[0] if len(parts) > 0 else "",
+                'unit': parts[1] if len(parts) > 1 else "",
+                'name': parts[2] if len(parts) > 2 else ""
+            })
+
+        r.ingredients = formatted_ingredients
+     ########--------------------------############## 
+    return render_template("recipes.html", recipes=recipes)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 @app.route('/create_recipe', methods=['GET','POST'])
 def create_recipe():
@@ -17,6 +42,7 @@ def create_recipe():
         return render_template('create_recipe.html', form=form)
 
     if form.validate_on_submit():
+        #This code will also be changed in order to conform to the database, right now it is just old code
         ing_data = [f"{i['amount']} {i['unit']} {i['ingredient_name']}" for i in form.ingredients.data]
         ing_string = " | ".join(ing_data)
 
@@ -25,14 +51,16 @@ def create_recipe():
             ingredients=ing_string,
             instructions=form.instructions.data
         )
-
+        ####-----------------##########
         db.session.add(new_recipe)
         db.session.commit()
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('home'))
 
     return render_template('create_recipe.html', form=form)
 
+#Page to view individual recipe
 @app.route('/recipe/<int:recipe_id>')
 def view_recipe(recipe_id):
     recipe = Recipe.query.get_or_404(recipe_id)
-    return render_template('recipe.html', recipe=recipe)
+    return render_template('view_recipe.html', recipe=recipe)
+
